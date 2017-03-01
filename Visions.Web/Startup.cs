652b832c3afed96a8a1ce0b3e1626205928bb183
pspassova <petya.spassova@abv.bg>
2017-03-01 15:15:28 +0200

    Initial

---
+++ Visions.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Visions.Web.Startup))]
+namespace Visions.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
