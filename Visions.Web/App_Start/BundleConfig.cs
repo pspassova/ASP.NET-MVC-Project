57d50a96405e4374d3862a80710a924ba122b84d
pspassova <petya.spassova@abv.bg>
2017-03-03 00:34:26 +0200

    Homepage styled up

--- Visions.Web/App_Start/BundleConfig.cs
+++ Visions.Web/App_Start/BundleConfig.cs
@@ -14,6 +14,12 @@ namespace Visions.Web
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/creative").Include(
+                        "~/Scripts/creative.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scrollreveal").Include(
+                        "~/Scirpts/scrollreveal.min.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -24,8 +30,14 @@ namespace Visions.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/vendor/font-awesome/css/font-awesome.css",
+                      "~/Content/vendor/bootstrap/css/bootstrap.css",
+                      "~/Content/css/creative.css"));
+
+            bundles.Add(new StyleBundle("~/Content/less").Include(
+                      "~/Content/less/creative.less",
+                      "~/Content/less/mixins.less",
+                      "~/Convent/less/variables.less"));
         }
     }
 }
