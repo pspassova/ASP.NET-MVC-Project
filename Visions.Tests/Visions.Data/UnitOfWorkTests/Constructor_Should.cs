9278230c484bcefd9d5cb6ccc542b5717c153fb3
pspassova <petya.spassova@abv.bg>
2017-03-03 17:21:08 +0200

    A test test

---
+++ Visions.Tests/Visions.Data/UnitOfWorkTests/Constructor_Should.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using Visions.Data;
+
+namespace Visions.Tests.Visions.Data.UnitOfWorkTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void ThrowArgumentNullException_WhenIVisionsDbContextIsNull()
+        {
+            // Arrange, Act
+            var exception = Assert.Throws<ArgumentNullException>(() => new UnitOfWork(null));
+
+            // Assert
+            StringAssert.IsMatch("context", exception.ParamName);
+        }
+    }
+}
